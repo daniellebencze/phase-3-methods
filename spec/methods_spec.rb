474c1f47d38edd63f3728d76316b979140fd5969
Danielle Bencze <daniellebencze@live.com>
2022-02-20 16:22:53 -0800

    commit tree b7ae6b00e6c3222353d190021a80f3792bad7da7

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -52,3 +52,27 @@ describe '#halve' do
   end
 
 end
+
+
+def greet_programmer() 
+puts "Hello, programmer!"
+end 
+
+def greet(name)
+ puts "Hello, #{name}!"
+end
+
+ def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(num1 = 2, num2= 5)
+  num1 + num2
+end
+
+def halve(num = 6)
+   return nil unless num.class == Integer
+
+   num / 2
+end
+
